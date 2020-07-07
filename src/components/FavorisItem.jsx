25492ae37980776706c3f51b78907b075a06ef34
Emeline <emeline.garo@supinternet.fr>
2020-07-07 15:53:09 +0200

    add favitem

--- src/components/FavorisItem.jsx
+++ src/components/FavorisItem.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const FavorisItem = () => {
-    return (
-        
-    );
-}
+  return <div>Domicile</div>;
+};
 
 export default FavorisItem;
