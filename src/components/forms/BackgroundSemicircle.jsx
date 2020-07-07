64934458484038b67b9f020946751899537d4eda
Wood <wassim.boulatika@supinternet.fr>
2020-07-07 18:07:12 +0200

    removed Title component, the Title is now in the BackgroundSemicircle component, removed margin in all the app and added a component Margin instead

--- src/components/forms/BackgroundSemicircle.jsx
+++ src/components/forms/BackgroundSemicircle.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 
-const Text = styled.text`
-  background-color: #707070;
-  margin: -20px -20px 0 -20px;
-  height: 86px;
-  border-radius: 0 0 100% 100%;
-  width: 100%;
-  position: absolute;
-  z-index: -1;
-  opacity: 0.86;
+const Text = styled.h1`
+    text-decoration: none;
+    color: #fff;
+    font-size: 32px;
+    text-align: center;
+    margin-bottom: 45px;
 `;
 
-const BackgroundColor = styled.text`
-  background-color: #707070;
+const BackgroundColor = styled.div`
+    background-color: #707070;
+    margin: -20px 0px 20px 0px;
+    height: 86px;
+    border-radius: 0 0 100% 100%;
+    z-index: -1;
+    opacity: 0.86;
 `;
 
 const BackgroundSemicircle = ({ text }) => {
-  return <Text style={{ color: BackgroundColor }}>{text}</Text>;
+  return(
+    <BackgroundColor>
+      <Text>{text}</Text>
+    </BackgroundColor>
+  );
 };
 
 export default BackgroundSemicircle;
