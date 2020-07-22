e9278eab5758748ef2afe595f57748110926d1f6
Wood <wassim.boulatika@supinternet.fr>
2020-07-22 18:06:02 +0200

    added disabled button component

---
+++ src/components/ButtonProfileDisabled.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaChevronRight } from "react-icons/fa";
+
+import Themes from "../constants/Themes";
+
+const ButtonProfileDisabled = ({ text = "", navigation, ...otherProps }) => {
+  return (
+    <Themes.ButtonProfileDisabled
+      to={`${navigation}`}
+      className="btn btn-warning"
+      type="text"
+      {...otherProps}
+    >
+      {text} <FaChevronRight className="chevron-right" />
+    </Themes.ButtonProfileDisabled>
+  );
+};
+
+export default ButtonProfileDisabled;
