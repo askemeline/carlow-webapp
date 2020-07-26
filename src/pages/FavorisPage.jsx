8972a0b810a2c567e7a2f8b934e72429516f1b0a
Maxime Maréchal <maxime.marechal@supinternet.fr>
2020-07-26 17:23:41 +0200

    added get fav

--- src/pages/FavorisPage.jsx
+++ src/pages/FavorisPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import Margin from "../components/forms/Margin";
 import HeaderButton from "../components/forms/HeaderButton";
@@ -6,12 +7,18 @@ import TabBarBottom from "../components/TabBarBottom";
 import Themes from "../constants/Themes";
 
 const FavorisPage = () => {
+  const getFavs = async () => {
+    const { data: response } = await axios.get(
+      "https://api-carlow.herokuapp.com/api/favs"
+    )
+    console.log(response);    
+  }
+  getFavs()
   return (
     <>
       <Margin heightProps="70%">
         <HeaderButton icon="back" text="Retour" navigation="home" />
         <Themes.Text style={{ fontWeight: "bold" }}>Vos favoris</Themes.Text>
-        <>Domicile : </>
       </Margin>
       <TabBarBottom text="home" />
     </>
