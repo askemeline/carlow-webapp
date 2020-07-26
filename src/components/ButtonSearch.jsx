8497feb72f43c0fb3c5771dffe1fd2283a033403
Emeline <emeline.garo@supinternet.fr>
2020-07-27 00:20:05 +0200

    improve style

--- src/components/ButtonSearch.jsx
+++ src/components/ButtonSearch.jsx
@@ -1,55 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 import { BsSearch } from "react-icons/bs";
-import { IconContext } from "react-icons";
+import Themes from "../constants/Themes.js";
 
-const Input = styled.button`
-  width: 100%;
-  padding: 15px 10px;
-  margin: 8px 0;
-  box-sizing: border-box;
-  border-radius: 15px;
-  border: 1px solid #f9f9f9;
-  box-shadow: 0px 0px 19px -9px rgba(0, 0, 0, 0.48);
-  height: 70px;
-
-  ::placeholder {
-    color: #273539;
-  }
-`;
 const Flex = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
-const Text = styled.p`
-  font-size: 16px;
-  margin: 15px 0;
-  color: #000;
-  font-weight: 400;
-`;
-
-const Line = styled.div`
-  width: 1px;
-  min-height: 40px;
-  background: #000;
-  margin-right: 20px;
-`;
 
 const ButtonSearch = ({ name, ...otherProps }) => {
   return (
     <div>
-      <Input name={name} {...otherProps}>
+      <Themes.ButtonSearch name={name} {...otherProps}>
         <Flex>
-          <Text>Votre destintaion</Text>
+          <Themes.Text style={{ fontWeight: 400 }}>
+            Votre destintaion
+          </Themes.Text>
           <Flex>
-            <Line />
-            <IconContext.Provider value={{ size: "1.5em" }}>
+            <Themes.Line />
+            <Themes.Div>
               <BsSearch />
-            </IconContext.Provider>
+            </Themes.Div>
           </Flex>
         </Flex>
-      </Input>
+      </Themes.ButtonSearch>
     </div>
   );
 };
--- src/components/TabBarBottom.jsx
+++ src/components/TabBarBottom.jsx
@@ -1,89 +1,49 @@
 import React from "react";
-import styled from "styled-components";
 import { MdSettings } from "react-icons/md";
 import { IoIosHome } from "react-icons/io";
-import { IconContext } from "react-icons";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-const Container = styled.div`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  height: 80px;
-  /* margin: 0 -20px; */
-  font-size: 16px;
-  box-shadow: inset 0px 11px 8px -10px #ccc, inset 0px -11px 8px -10px #ccc;
-  justify-content: space-around;
-`;
-const Flex = styled.div`
-  display: flex;
-  align-items: center;
-  height: 50px;
-  width: 120px;
-  display: flex;
-  justify-content: center;
-  border-radius: 25px;
-`;
-const Text = styled.p`
-  margin-left: 5px;
-  text-decoration: none;
-`;
+import Themes from "../constants/Themes.js";
 
-const TabBarBottom = ({ text }) => {
+const TabBarBottom = () => {
+  let location = useLocation();
   return (
-    <Container>
-      {text === "profile" ? (
+    <Themes.ContainerTabBarBottom>
+      {location.pathname === "/profile" ? (
         <>
           <Link to="/home">
-            <IconContext.Provider value={{ color: "#022A2F", size: "2em" }}>
+            <Themes.IconOnlyabBar>
               <IoIosHome />
-            </IconContext.Provider>
+            </Themes.IconOnlyabBar>
           </Link>
           <Link to="/profile" style={{ textDecoration: "none" }}>
-            <Flex
-              style={{
-                backgroundColor: text === "home" ? "#F3D2D3" : "#B4D0D3",
-              }}
-            >
-              <IconContext.Provider value={{ color: "#337F86", size: "2em" }}>
+            <Themes.FlexSetting>
+              <Themes.IconSettingTabBar>
                 <MdSettings />
-                <Text
-                  style={{ color: text === "home" ? "#AA696B" : "#337F86" }}
-                >
-                  Réglage
-                </Text>
-              </IconContext.Provider>
-            </Flex>
+                <Themes.TextSettingTabBar>Réglage</Themes.TextSettingTabBar>
+              </Themes.IconSettingTabBar>
+            </Themes.FlexSetting>
           </Link>
         </>
       ) : (
         <>
           <Link to="/home" style={{ textDecoration: "none" }}>
-            <Flex
-              style={{
-                backgroundColor: text === "home" ? "#F3D2D3" : "#B4D0D3",
-              }}
-            >
-              <IconContext.Provider value={{ color: "#AA696B", size: "2em" }}>
+            <Themes.FlexHome>
+              <Themes.IconHomeTabBar>
                 <IoIosHome />
-                <Text
-                  style={{ color: text === "home" ? "#AA696B" : "#337F86" }}
-                >
-                  Accueil
-                </Text>
-              </IconContext.Provider>
-            </Flex>
+                <Themes.TextHomeTabBar>Accueil</Themes.TextHomeTabBar>
+              </Themes.IconHomeTabBar>
+            </Themes.FlexHome>
           </Link>
           <Link to="/profile">
-            <IconContext.Provider value={{ color: "#022A2F", size: "2em" }}>
+            <Themes.IconOnlyabBar>
               <MdSettings />
-            </IconContext.Provider>
+            </Themes.IconOnlyabBar>
           </Link>
         </>
       )}
-    </Container>
+    </Themes.ContainerTabBarBottom>
   );
 };
 
